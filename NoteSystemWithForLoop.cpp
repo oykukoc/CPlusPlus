adb45df96f5ee2b9dcb6fc0ea350f030188d4932
oykukoc <oykukoc07@hotmail.com>
2025-11-07 21:58:00 +0100

    updated

--- NoteSystemWithForLoop.cpp
+++ NoteSystemWithForLoop.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 int main(){
 
-    int grade;
+    int countOfStudents;
+    cout << "How Many Stundents? ";
+    cin >> countOfStudents;
 
-    //cout << "Please enter your grade here: ";
-    //cin >> grade;
+    for(int i = 0 ; i < countOfStudents ; i++){
 
-    do{
+        int grade;
+
         cout << "Please enter your grade here: ";
         cin >> grade;
-        
-        if(grade > 100 || grade < 0 ){
-            cout << "Please enter a valid grade!" << endl;
-        }else if(grade >= 90){
+
+        while(grade > 100 || grade < 0 ){
+            cout << "Please enter a valid grade here! : ";
+            cin >> grade;
+        }
+
+        if(grade >= 90){
             cout << "You got AA!" << endl;
         }else if(grade >= 80){
             cout << "You got BA!"<< endl;
@@ -28,9 +33,9 @@ int main(){
             cout << "You got CC" << endl;
         }else{
             cout << "You could not pass" << endl;
-        }  
+        }
+
     }
-    while(grade > 100 || grade < 0  );
 
     return 0;
 }
